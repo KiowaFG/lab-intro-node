e98fc93e34fe3ba051dffd4697e38185147b0793
kiowaFG <kiowacdq@gmail.com>
2024-04-29 16:50:50 +0200

    lab done

--- index.js
+++ index.js
@@ -1,17 +1,74 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = []
+    this.length = this.items.length 
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item)
+    this.length = this.items.length 
+    this.items.sort((a,b)=>{
+      return a-b
+    })
+  
+  }
 
-  get(pos) {}
+  get(pos) {
 
-  max() {}
+    
+    if (pos > this.items.length ){
+      throw new Error("OutOfBounds"); 
+    
+    }
+    else {
+      return this.items[pos]
+    }
+  }
 
-  min() {}
+  max() {
+    if (this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
 
-  sum() {}
+    else{
+      return (this.items[this.items.length -1])
+    }
+  }
 
-  avg() {}
+  min() {
+    if (this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+
+    else{
+      return (this.items[0])
+    }
+  }
+
+  sum() {
+      let total = 0;
+      this.items.forEach((element) => {
+        total += element  
+      });
+      return total
+      
+    }
+  
+
+  avg() {
+    let total = 0;
+    this.items.forEach((element) => {
+      total += element  
+    });
+    let average = total / this.items.length
+  
+
+    if (this.items.length === 0){
+      throw new Error("EmptySortedList");
+    }
+    return average
+
+  }
 }
 
 module.exports = SortedList;
